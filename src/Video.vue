e3585c408b45ddeaf23e2dc677c39612e1300da0
Michael Tran <solidwater969@gmail.com>
2022-04-24 15:15:23 -0400

    refactor: update css in video

--- src/Video.vue
+++ src/Video.vue
@@ -2,16 +2,40 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+  methods: {
+    redirect() {
+      window.open('https://www.youtube.com/watch?v=' + this.video.id.videoId, '_blank')
+    }
+  },
   props: ['video']
 })
-// 
 </script>
 
 <template>
-  <p>{{ 'https://www.youtube.com/watch?v=' + video.id.videoId }}</p>
-  <p>{{ video.snippet.thumbnails.medium.url }}</p>
-  <p>{{ video.snippet.title }}</p>
-  <p>{{ video.snippet.publishTime }}</p>
-  <p>{{ video.snippet.channelTitle }}</p>
-  <p>{{ video.snippet.description }}</p>
+  <div class="hover mb-3" @click="redirect" tabindex="0">
+    <div class="row mx-0">
+      <div class="col-1"></div>
+      <div class="col-4 d-flex justify-content-end">
+        <img :src="video.snippet.thumbnails.medium.url" class="img-thumbnail">
+      </div>
+      <div class="col-6">
+        <h5 class="py-1">{{ video.snippet.title }}</h5>
+        <p>Date: {{ video.snippet.publishTime }}</p>
+        <p>Channel: {{ video.snippet.channelTitle }}</p>
+        <p>Description: {{ video.snippet.description }}</p>
+      </div>
+      <div class="col-1"></div>
+    </div>
+  </div>
 </template>
+
+<style>
+.hover {
+  cursor: pointer;
+  transition: 250ms;
+}
+
+.hover:hover {
+  box-shadow: 10px 10px 10px 10px red;
+}
+</style>
